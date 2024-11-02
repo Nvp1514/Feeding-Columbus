d77b9426cdb204f36ec44be91cf3f1ac4ef2200e
Natasha Patel <123392010+Nvp1514@users.noreply.github.com>
2024-11-02 13:31:03 -0400

    Update app.py
    Testing URL

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 def index():
   return render_template('index.html')
 
-@app.route('/about', methods=["GET", "HEAD"])
+@app.route('/about.html', methods=["GET", "HEAD"])
 def about():
   return render_template('about.html')
 
