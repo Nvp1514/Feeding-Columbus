a77dd41b1ca0c7beebf0b1be2607b30cccf55557
Natasha Patel <123392010+Nvp1514@users.noreply.github.com>
2024-11-02 13:39:22 -0400

    Update app.py
    Update URL for everything on the nav bar

--- app.py
+++ app.py
@@ -6,9 +6,29 @@ app = Flask(__name__)
 def index():
   return render_template('index.html')
 
+@app.route('/index.html', methods=["GET","HEAD"])
+def index2():
+  return render_template('index.html')
+
 @app.route('/about.html', methods=["GET", "HEAD"])
 def about():
   return render_template('about.html')
+
+@app.route('/volunteer.html', methods=["GET","HEAD"])
+def volunteer():
+  return render_template('volunteer.html')
+
+@app.route('/events.html', methods=["GET","HEAD"])
+def events():
+  return render_template('events.html')
+
+@app.route('/contact.html', methods=["GET","HEAD"])
+def contact():
+  return render_template('contact.html')
+
+@app.route('/donate.html', methods=["GET","HEAD"])
+def donate():
+  return render_template('donate.html')
 
 
 if __name__ == '__main__':
