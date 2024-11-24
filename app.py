b21b135c7b8ca560308372bed4f9c384f7c9746e
Natasha Patel <123392010+Nvp1514@users.noreply.github.com>
2024-11-24 15:26:15 -0500

    Update app.py

--- app.py
+++ app.py
@@ -7,6 +7,13 @@ app = Flask(__name__)
 
 uri = "mongodb+srv://natashapatel2015:<db_password>@cluster0.atpjq.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
 client = MongoClient(uri, server_api=ServerApi('1'))
+
+try:
+    client.admin.command('ping')
+    print("Pinged your deployment. You successfully connected to MongoDB!")
+except Exception as e:
+    print(e)
+
 db = client['FCdatabase']
 collection = db['FCcollection']
 
